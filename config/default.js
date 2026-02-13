915381df28f51f8bde19154f89165ab7d800918f
Sergei Osokin <sergei.osokin.rf@mail.ru>
2026-02-13 21:31:24 +0300

    Fix tls

--- config/default.js
+++ config/default.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'develop',
   TLG_TOKEN: process.env.TLG_TOKEN || process.env.TLG_TOKEN,
-  OPENAI_API_KEY: "-"
+  HOST: process.env.HOST || 'http://localhost:3000/api/tlg/',
 };
--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+const { HOST } = require("../config/default");
+
 const request = async (url, method, form = {}) => {
     console.log(form)
     const header = {
@@ -8,13 +10,17 @@ const request = async (url, method, form = {}) => {
     const params = method === 'GET' ?
         { method, headers: header, body } : { method, body, headers: header, credentials: 'include' };
     try {
-        const response = await fetch(`http://localhost:3000/api/tlg/${url}`, params);
+        const response = await fetch(`${HOST}${url}`, params);
         const data = await response.json();
+
+        if (responce.hasOwnProperty('error')) {
+            await ctx.reply(responce.message || responce.error, keyboardError);
+            return;
+        };
 
         return data;
     } catch (error) {
-        return error
-        // message(error, false);
+        return ctx.reply('Попробуйте заново. У нас тут ошибка ⚙️', keyboardError);
     }
 };
 
